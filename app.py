5f6a55e5049e9569545ff8bc3d99c65d0d2915be
Boutzi <joe@arkaans.com>
2024-09-15 18:01:54 +0200

    feat(swagger):namespace

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, abort
-from flask_restx import Api, Resource
+from flask_restx import Api, Resource, Namespace
 import os
 from mongodb_client import AtlasClient
 from dotenv import load_dotenv
@@ -17,11 +17,14 @@ app = Flask(__name__)
 api = Api(app, version='1.0', title='PyGo Score API',
           description='A simple API for managing player scores in PyGo')
 
+player_ns = Namespace('Players', description='Players management')
+app.config['SWAGGER_UI_DOC_EXPANSION'] = 'list'
+
 atlas_client = AtlasClient(ATLAS_URI, DB_NAME)
 atlas_client.ping()
 print('Connected to Atlas instance! We are good to go!')
 
-@api.route('/leaderboard')
+@player_ns.route('/leaderboard')
 class Leaderboard(Resource):
     def get(self):
         players = atlas_client.find(COLLECTION_NAME, {}, limit=0)
@@ -29,7 +32,7 @@ class Leaderboard(Resource):
             player['_id'] = str(player.get('_id')) 
         return players
 
-@api.route('/players/<int:playerId>')
+@player_ns.route('/players/<int:playerId>')
 class Player(Resource):
     def get(self, playerId):
         player = atlas_client.find(COLLECTION_NAME, {"id": playerId}, limit=1)
@@ -38,8 +41,8 @@ class Player(Resource):
             player['_id'] = str(player.get('_id')) 
             return player
         abort(404, description="Player not found")
-
-@api.route('/players/<string:player>/<int:best>')
+        
+@player_ns.route('/players/<string:player>/<int:best>')
 class PlayerBestScore(Resource):
     def put(self, player, best):
         player_data = atlas_client.find(COLLECTION_NAME, {"name": player.upper()})
@@ -54,6 +57,9 @@ class PlayerBestScore(Resource):
             }
             atlas_client.database[COLLECTION_NAME].insert_one(new_player)
         return {"success": True}
+
+
+api.add_namespace(player_ns)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
